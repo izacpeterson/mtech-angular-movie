69e678bb02ebcdba11573958f8f566553c319069
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-04-19 08:58:16 -0600

    added firebaseConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA1RGOcLKmaeYwosNvyxXYVzlKLC7kV2k8",
+    authDomain: "mtech-movie-2.firebaseapp.com",
+    projectId: "mtech-movie-2",
+    storageBucket: "mtech-movie-2.appspot.com",
+    messagingSenderId: "514645146266",
+    appId: "1:514645146266:web:8bc4d6bbf5834b53387d23"
+  }
 };
 
 /*

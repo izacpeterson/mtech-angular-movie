9f72383d6bb526267d3831d2355e2310fd3a8f20
izacpeterson <izacpeterson@gmail.com>
2022-04-20 09:28:59 -0600

    setup google login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyA1RGOcLKmaeYwosNvyxXYVzlKLC7kV2k8",
-    authDomain: "mtech-movie-2.firebaseapp.com",
-    projectId: "mtech-movie-2",
-    storageBucket: "mtech-movie-2.appspot.com",
-    messagingSenderId: "514645146266",
-    appId: "1:514645146266:web:8bc4d6bbf5834b53387d23"
-  }
+    apiKey: 'AIzaSyA1RGOcLKmaeYwosNvyxXYVzlKLC7kV2k8',
+    authDomain: 'mtech-movie-2.firebaseapp.com',
+    projectId: 'mtech-movie-2',
+    storageBucket: 'mtech-movie-2.appspot.com',
+    messagingSenderId: '514645146266',
+    appId: '1:514645146266:web:8bc4d6bbf5834b53387d23',
+  },
 };
 
 /*

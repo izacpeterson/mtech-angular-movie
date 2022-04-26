6b70e057b1cc25248edf3948ca50968a33fd86b3
Izac Peterson <izac@localhost.localdomain>
2022-04-26 10:18:59 -0600

    navbar name

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private userService: UserService) {}
+  navName = '';
   ngOnInit(): void {
+    this.userService.getUserName.subscribe((name: any) => {
+      this.navName = name;
+    });
   }
-
 }

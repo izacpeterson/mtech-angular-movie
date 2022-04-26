9091e0d7940ce27fadb7999097eef94bfc34b85e
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-04-26 10:34:44 -0600

    added a scroll up button with animations

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,11 +2,35 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { ViewportScroller } from '@angular/common';
 import { map, take } from 'rxjs';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate('.2s ease-out',
+              style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('.2s ease-in',
+              style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class SearchComponent implements OnInit {
   hasResults: boolean = true;
@@ -14,6 +38,7 @@ export class SearchComponent implements OnInit {
   searchInput: string = '';
   displayedMovies: any = [];
   pageCount: number = 1;
+  showScrollBtn: boolean = false;
 
   constructor(
     private apiService: ApiService,
@@ -26,9 +51,8 @@ export class SearchComponent implements OnInit {
     this.apiService.discoverMovies(this.pageCount++).pipe(
       map((res: any) => {
         res.results.forEach((movie: any) => {
-          this.displayedMovies.push(movie)
+          this.displayedMovies.push(movie);
         })
-        console.log('movie info', this.displayedMovies);
       })
     ).subscribe()
     this.loading = false;
@@ -37,8 +61,15 @@ export class SearchComponent implements OnInit {
   @HostListener('window:scroll', ['$event'])
   getScrollHeight() {
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-      this.getMoreMovies()
+      this.getMoreMovies();
     }
+    if (window.scrollY > 100) {
+      this.showScrollBtn = true
+    }
+    else {
+      this.showScrollBtn = false
+    }
+
   }
 
   getSearch() {
@@ -69,9 +100,8 @@ export class SearchComponent implements OnInit {
     this.apiService.discoverMovies(this.pageCount++).pipe(take(1),
       map((res: any) => {
         res.results.forEach((movie: any) => {
-          this.displayedMovies.push(movie)
+          this.displayedMovies.push(movie);
         })
-        console.log('movie info', this.displayedMovies);
       })
     ).subscribe()
   }

7a1f6e2ce854005781e34d4218a2da67df87a076
sherylmoser <sherylrmoser@gmail.com>
2022-04-28 09:47:57 -0600

    add to favorites done

--- src/app/services/firebase-api.service.ts
+++ src/app/services/firebase-api.service.ts
@@ -43,6 +43,12 @@ export class FirebaseApiService {
     })
   }
 
+  async addToFavorites(movieId: number, uid: string) {
+    await updateDoc(doc(this.db, 'users', uid), {
+      favorites: arrayUnion(movieId)
+    })
+  }
+
   calculateAverageRating(movieId: number) {
 
   }

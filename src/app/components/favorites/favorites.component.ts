354fd6aea767df791948a52fd25b04a730492e61
JaxonKnight28 <jaxon.knight2725@stu.mtec.edu>
2022-04-29 08:34:36 -0600

    started the favorites page, firebase needs adjustment

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { FirebaseApiService } from 'src/app/services/firebase-api.service';
+import { UserService } from 'src/app/services/user.service'
 
 @Component({
   selector: 'app-favorites',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  currentUserId: string = '';
+  userData: any
+
+  watchList: any;
+  favorites: any;
+
+  constructor(
+    private userService: UserService,
+    private firebaseService: FirebaseApiService,
+    private apiService: ApiService,
+  ) { }
 
   ngOnInit(): void {
+
+    this.userService.getUID.subscribe((user: any) => {
+      this.currentUserId = user;
+      this.firebaseService.getUserById(user).then((data: any) => {
+        this.userData = data;
+        this.watchList = data.watchlist;
+        this.favorites = data.favorites;
+        //console.log("DATA", this.userData);
+      })
+    })
+
+    //this.apiService.getMovieDetails()
+
   }
+
+  test() {
+    console.log(this.userData);
+
+  }
+
+
 
 }

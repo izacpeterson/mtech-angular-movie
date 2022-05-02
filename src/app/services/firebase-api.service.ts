2619135a4c9dcf6f61b33baca4f164b3933e94db
Noah <noq@noq.local>
2022-05-02 08:23:19 -0600

    added comments query

--- src/app/services/firebase-api.service.ts
+++ src/app/services/firebase-api.service.ts
@@ -4,22 +4,28 @@ import { HttpClient } from '@angular/common/http';
 // Firebase modules
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import {
-  collection, getFirestore, arrayUnion,
-  doc, setDoc, updateDoc, getDoc, getDocs,
-  query, where
+  collection,
+  getFirestore,
+  arrayUnion,
+  doc,
+  setDoc,
+  updateDoc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+  onSnapshot,
+  arrayRemove,
 } from 'firebase/firestore';
 
 import { User } from 'src/app/interfaces/user';
 import { environment } from 'src/environments/environment';
-
+import { Observable, Subscriber } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseApiService {
-
-  // app: any;
-  // db: any;
   app = initializeApp(environment.firebaseConfig);
   db = getFirestore(this.app);
 
@@ -29,99 +35,161 @@ export class FirebaseApiService {
 
   constuctor() {}
 
-
   // initialize firebase app
-  async init() {
-
-    // // only initialize app if app does not exist
-    // this.app = getApps().length ? getApp() : initializeApp(this.firebaseConfig);
-
-    // // database refrence
-    // this.db = getFirestore(this.app);
-
-
+  async init() {  // note that some values may be uninitialized when referenced (async init)
     // collection refrences
     this.usersRef = collection(this.db, 'users');
     this.moviesRef = collection(this.db, 'movies');
     this.commentsRef = collection(this.db, 'comments');
 
-    getDocs(this.usersRef).then(querySnapshot => {
-      console.log(querySnapshot);
-      querySnapshot.forEach(doc => {
-        console.log(doc.id, " => ", doc.data());
-      });
-      console.log("---");
+    this.getComments('438631').then(comments => {
+      console.log(comments);
     });
-
-    this.getUserById('uVxllDLwVVrBjKNIGfub').then(data => {
-      console.log(data);
-    });
-
-    this.getUserByHandle('johndoe');
-
   }
 
 
   //
-  // read queries
+  // user search queries
   //
 
   async getUserById(id: string) {
     const user = await getDoc(doc(this.db, 'users', id));
-    return user.data();
-  }
-
-  async getCommentById(id: string) {
-    const comment = await getDoc(doc(this.db, 'comments', id));
-    return comment.data();
-  }
-
-  async getCommentsByMovieId(id: string) {
-  }
-
-  async getRatingsByMovieId(id: string) {
+    if (user.exists()) {
+      return user.data();
+    }
+    return undefined;
   }
 
   async getUserByHandle(handle: string) {
     const querySnapshot = await getDocs(
       query(this.usersRef, where('handle', '==', handle))
     );
-    return querySnapshot.docs[0].data();
+    if (querySnapshot.docs[0]) {
+      return querySnapshot.docs[0].data();
+    }
+    return undefined;
   }
 
-  calculateAverageRating(movieId: number) {
 
+  //
+  // watchlist/favorites
+  //
+
+  async addToWatchList(
+    movieId: number,
+    uid: string,
+    posterPath: string,
+    movieTitle: string
+  ) {
+    await updateDoc(doc(this.db, 'users', uid), {
+      watchlist: arrayUnion({
+        movieId: movieId,
+        posterPath: posterPath,
+        movieTitle: movieTitle,
+      }),
+    });
+  }
+
+  async addToFavorites(
+    movieId: number,
+    uid: string,
+    posterPath: string,
+    movieTitle: string
+  ) {
+    await updateDoc(doc(this.db, 'users', uid), {
+      favorites: arrayUnion({
+        movieId: movieId,
+        posterPath: posterPath,
+        movieTitle: movieTitle,
+      }),
+    });
+  }
+
+  async deleteFromWatchlist(
+    uid: any,
+    movieId: number,
+    movieTitle: string,
+    posterPath: string
+  ) {
+    await updateDoc(doc(this.db, 'users', uid), {
+      watchlist: arrayRemove({
+        movieId: movieId,
+        movieTitle: movieTitle,
+        posterPath: posterPath,
+      }),
+    });
+  }
+
+  async deleteFromFavorites(
+    uid: any,
+    movieId: number,
+    movieTitle: string,
+    posterPath: string
+  ) {
+    await updateDoc(doc(this.db, 'users', uid), {
+      favorites: arrayRemove({
+        movieId: movieId,
+        movieTitle: movieTitle,
+        posterPath: posterPath,
+      }),
+    });
+  }
+
+
+  //
+  // comments
+  //
+
+  // return movie.comments array
+  async getComments(movieId: string, limit: number = 20) {
+    const movie = await getDoc(doc(this.db, 'movies', movieId));
+    if (movie.exists()) {
+      return movie.data().comments;
+    }
+    return undefined;
+  }
+
+  // append comment to movies.comments array
+  async addToComments(movieId: string, username: any, comment: any) {
+    await updateDoc(doc(this.db, 'movies', movieId), {
+      comments: arrayUnion({
+        username: username,
+        comment: comment,
+      }),
+    });
   }
 
   //
-  // write queries
+  // ratings
   //
 
-  async addToWatchList(movieId: number, uid: string) {
-    await updateDoc(doc(this.db, 'users', uid), {
-      watchlist: arrayUnion(movieId)
-    })
+  // set avg movie rating to custom value
+  async setPublicRating(movieId: string, rating: number) {}
+
+  // fetch all movie reviews and automatically set correct movie rating in database
+  async recalculatePublicRating(movieId: string) {}
+
+  async addRating(movieId: string, value: number) {
+    await updateDoc(doc(this.db, 'movies', movieId), {
+      ratings: arrayUnion(value),
+    });
   }
 
-  async addToFavorites(movieId: number, uid: string) {
-    await updateDoc(doc(this.db, 'users', uid), {
-      favorites: arrayUnion(movieId)
-    })
+  async getRating(movieId: string, callback: Function) {
+    let average: number = 0;
+    const docRef = doc(this.db, 'movies', movieId);
+
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      let data = docSnap.data().ratings;
+      let total = 0;
+      data.forEach((value: number) => {
+        total += value;
+      });
+      average = total / data.length;
+      callback(average);
+    }
   }
-
-
-  async setPublicRating(movieId: string, rating: number) {
-
-  }
-
-  async recalculatePublicRating(movieId: string) {
-
-  }
-
-  async addRating(movieId: string, userId: string, value: number) {
-
-  }
-
-
 
 }
